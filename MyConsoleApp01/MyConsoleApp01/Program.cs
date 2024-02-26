ecc11b86edab653d41d836564e5b79cf59017470
zh-lan <zhangl@hotmail.com>
2024-02-25 19:37:04 -0500

    Add VS solution folder

---
+++ MyConsoleApp01/MyConsoleApp01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> prodList = GetProductList();
+
+            foreach (Product p in prodList)
+            {
+                Console.WriteLine($"{p.ProductCode} | {p.ProductName} | {p.Price}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static List<Product> GetProductList()
+        {
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product("A01", "Product A01", 2.99));
+            productList.Add(new Product("A02", "Product A02", 3.99));
+            productList.Add(new Product("B01", "Product B01", 12.99));
+            productList.Add(new Product("B02", "Product B02", 22.99));
+            productList.Add(new Product("B03", "Product B01", 44.99));
+
+            return productList;
+        }
+    }
+
+    public class Product
+    {
+        public Product(string code, string name, double price)
+        {
+            ProductCode = code;
+            ProductName = name;
+            Price = price;
+        }
+
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+    }
+}
